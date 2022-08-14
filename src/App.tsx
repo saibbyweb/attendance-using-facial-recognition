e0c638b85fc3d482331e584613bf79c0f4fd8df6
Suhaib Khan <hello@saibbyweb.com>
2022-08-15 00:58:46 +0530

    admin panel base ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
+
 import Faculty from '@/pages/Faculty'
 import Homepage from '@/pages/Homepage'
 import ResponsiveAppBar from '@/components/AppBar'
 import Student from '@/pages/Student'
 import Admin from '@/pages/Admin'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#a38080',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#e5e0e0',
+    },
+  },
+});
+
 function App() {
 
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <ResponsiveAppBar />
       <Routes>
@@ -18,6 +39,8 @@ function App() {
         <Route path="admin" element={<Admin />} />
       </Routes>
     </div>
+    </ThemeProvider>
+    
   )
 }
 
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,3 +1,43 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PhoneIcon from '@mui/icons-material/Phone';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+
+function TabPanel({ value, index, children }: any) {
+    return (<div hidden={value !== index}> {children} </div>)
+}
+
 export default function Admin() {
-    return (<h1> Admin </h1>)
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (<>
+        <div className="sidebar-menu">
+            <Tabs textColor="inherit" indicatorColor="primary" variant="fullWidth" orientation="vertical" value={value} onChange={handleChange} aria-label="icon label tabs example">
+                <Tab icon={<PhoneIcon />} label="RECENTS" />
+                <Tab icon={<FavoriteIcon />} label="FAVORITES" />
+                <Tab icon={<PersonPinIcon />} label="NEARBY" />
+            </Tabs>
+
+ 
+        </div>
+        {/* content */}
+        <div className="content">
+        <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+        </div>
+    </>)
 }
