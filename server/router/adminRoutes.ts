3134a1c22e8d9fcf2447804437b1455392848ec3
Suhaib Khan <hello@saibbyweb.com>
2022-09-02 16:01:44 +0530

    crud api in process

--- server/router/adminRoutes.ts
+++ server/router/adminRoutes.ts
@@ -1,13 +1,44 @@
-import { server } from "@server/helpers/essentials"
+import { server, db } from "@server/helpers/essentials";
 import { PostReq, Res } from "@server/types/global";
+import { classes, Class } from "@server/models/class";
 /* creating express router */
 const router = server.express.Router();
 
+type ModelDoc = Class | unknown;
+
+/* crud request  */
+type CRUDRequestBody = {
+  operation: "read" | "add" | "update" | "delete";
+  modelName: "class" | string;
+  payload: ModelDoc;
+};
+
+/* crud response */
+type CRUDResponse = {
+  docs?: Array<ModelDoc>,
+  newDocument?: unknown
+};
 
 /* crud api */
-router.post('/crud', async (req: PostReq<{}>, res: Res<{}>) => {
+router.post("/crud", async (req: PostReq<CRUDRequestBody>, res: Res<CRUDResponse>) => {
+  let response: CRUDResponse = {};
+  /* extract operation, modelName & payload */
+  const { operation, modelName, payload } = req.body;
+  
+  switch (operation) {
+    case "read":
+      response.docs = await db.model(modelName).find();
+      break;
+    case "add":
+      const model = db.model(modelName);
+      await new model(payload).save();
+      break;
+    case "update":
+      break;
+    case "delete":
+      break;
+  }
 
-    
-})
-export default {router};
-
+  res.send({ msg: "Done", data: response });
+});
+export default { router };
